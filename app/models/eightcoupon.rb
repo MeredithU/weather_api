6ea1a66c21f245319e06ce4bfdb80adddb210d60
bvuongx <bvuong@gmail.com>
2013-07-29 13:03:05 -0700

    Got the 8coupons api working, but I reached the hourly access limit.  Have to find a way to store the data so I only need to access it once.

--- app/models/eightcoupon.rb
+++ app/models/eightcoupon.rb
@@ -6,24 +6,8 @@ require 'json'
 
 attr_accessible :zip
 
-
-
   def local_deals
-    HTTParty.get("http://api.8coupons.com/v1/getdeals?key=49bf9fac2c055141e65a2d9bd03064a1cd06069f59b9941403b24ea60ffcb8296bb6d954a145f830ed072500d4d213f8&zip=#{zip}&mileradius=20&limit=1&orderby=radius&categoryid=2,6")
+    HTTParty.get("http://api.8coupons.com/v1/getdeals?key=49bf9fac2c055141e65a2d9bd03064a1cd06069f59b9941403b24ea60ffcb8296bb6d954a145f830ed072500d4d213f8&zip=#{zip}&mileradius=20&limit=3&orderby=radius&categoryid=2,6")
   end
-
-  def deal_title
-    local_deals'dealDiscountPercent'
-  end
-
-def NY_local_deals
-open("http://api.8coupons.com/v1/getdeals?key=49bf9fac2c055141e65a2d9bd03064a1cd06069f59b9941403b24ea60ffcb8296bb6d954a145f830ed072500d4d213f8&zip=#{zip}&mileradius=20&limit=1000&orderby=radius&categoryid=2,6") do |f|
-  json_string = f.read
-  parsed_json = JSON.parse(json_string)
-  location = parsed_json['dealTitle']['name']
-  here = parsed_json['address']['city']
-  print "Get this deal #{location} at #{here}\n"
-end
-end
 
 end
--- app/models/wunder_request.rb
+++ app/models/wunder_request.rb
@@ -11,6 +11,11 @@ class WunderRequest
     end
   end
 
+  def self.local_deals
+    HTTParty.get("http://api.8coupons.com/v1/getdeals?key=49bf9fac2c055141e65a2d9bd03064a1cd06069f59b9941403b24ea60ffcb8296bb6d954a145f830ed072500d4d213f8&zip=10001&mileradius=20&limit=1000&orderby=radius&categoryid=2,6")
+  end
+
+
   def self.response
     HTTParty.get("http://api.wunderground.com/api/80c09000360316c5/geolookup/conditions/q/WA/Seattle.json")
   end
